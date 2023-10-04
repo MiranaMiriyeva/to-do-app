31be216b5cb19879082c5766e03f773459dc1a65
MiranaMiriyeva <arzumiri20@gmail.com>
2023-10-04 09:12:03 +0400

    todo

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let usernameElem =  document.getElementById("username")
-let useremailElem =  document.getElementById("useremail")
-let userpasswordElem =  document.getElementById("userpassword")
-let adduserElem =  document.getElementById("adduser")
-let tbodyElem = document.querySelector("table tbody")
-let data = []
-let id = 0
+let usernameElem =  document.getElementById("username");
+let useremailElem =  document.getElementById("useremail");
+let userpasswordElem =  document.getElementById("userpassword");
+let adduserElem =  document.getElementById("adduser");
+let editElem = document.getElementById("edit");
+let searchElem = document.getElementById("search");
+let tbodyElem = document.querySelector("table tbody");
+let data = [];
+let findedUser;
+let id = 0;
 adduserElem.addEventListener("click",(event) => {
     event.preventDefault()
     data.push({
@@ -13,12 +16,12 @@ adduserElem.addEventListener("click",(event) => {
         useremail:  useremailElem.value, 
         userpassword:  userpasswordElem.value, 
     })
-    theUser()
+    theUser(data)
+    clear()
 }) 
 function deleteuser(param){
     data = data.filter(user => user.userid !== param )
-    console.log("asdfghj");
-   theUser()
+   theUser(data)
 }
 function theUser(){
     tbodyElem.innerHTML= ""
@@ -30,30 +33,40 @@ function theUser(){
         <td>${user.username}</td>
         <td>${user.useremail}</td>
         <td>
-            <button class="btn btn-secondary" id="updateuser">Update</button>
+            <button class="btn btn-secondary"  onclick="updateuser(${user.userid})">Update</button>
             <button class="btn btn-danger" onclick="deleteuser(${user.userid})">Delete</button>
         </td>
     </tr>`})
 }
 
-let udelem = document.getElementById("updateuser")
-udelem.addEventListener("click",() => {
-    console.log("dfghjkl")
-    // if(param=user.userid){
-    //     usernameElem.value =  user.username  
-    //     useremailElem.value = user.useremail 
-    //     userpasswordElem.value =  user.userpassword 
-    //   }
-    //   theUser()
-      }
-)
+function updateuser(param){
+   findedUser = data.find(user=>user.userid === param)
+  usernameElem.value = findedUser.username
+  useremailElem.value = findedUser.useremail
+  userpasswordElem.value = findedUser.userpassword
+  editElem.classList.remove("d-none")
+  adduserElem.classList.add("d-none")
+}
 
+editElem.addEventListener("click",(event)=>{
+    editElem.classList.add("d-none")
+    adduserElem.classList.remove("d-none")
+    event.preventDefault()
+    findedUser.username = usernameElem.value, 
+    findedUser.useremail =  useremailElem.value, 
+    findedUser.userpassword =  userpasswordElem.value, 
+    theUser(data)
+    clear()
+})
 
-// function updateuser(param){
-// if(param=user.userid){
-//   usernameElem.value =  user.username  
-//   useremailElem.value = user.useremail 
-//   userpasswordElem.value =  user.userpassword 
-// }
-// theUser()
-// }
+searchElem.addEventListener("keyup",(event)=>{
+let filtered = data.filter(user=>user.username.includes(event.target.value))
+theUser(filtered)
+// console.log(event.target.value)
+})
+
+function clear(){
+    usernameElem.value =  "";
+    useremailElem.value = "";
+    userpasswordElem.value =  ""; 
+}
